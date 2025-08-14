ff76c0ad2035b43c6cd5a61fb6d1e075d8d46232
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-08-14 16:14:15 -0300

    findAll implementation

--- src/application/Main2.java
+++ src/application/Main2.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.List;
 import java.util.Scanner;
 
 import model.dao.DaoFactory;
@@ -16,6 +17,12 @@ public class Main2 {
 		System.out.println("=== TEST 1: department findById ===");
 		Department department = departmentDao.findById(2);
 		System.out.println(department);
+		
+		System.out.println("\n=== TEST 2: department findAll ===");
+		List<Department> list = departmentDao.findAll();
+		for(Department dep: list) {
+			System.out.println(dep);
+		}
 		
 		sc.close();
 	}
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -65,17 +68,49 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 		}
 	}
 
+	@Override
+	public List<Department> findAll() {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+					"SELECT * FROM department "
+					+ "ORDER BY Name"
+					);
+			
+			rs = st.executeQuery();
+			
+			List<Department> list = new ArrayList<>();
+			Map<Integer, Department> map = new HashMap<>();
+			
+			while(rs.next()) {
+				
+				Department dep = map.get(rs.getInt("Id"));
+				
+				if(dep == null) {					
+					dep = instantiateDepartment(rs);
+					map.put(rs.getInt("Id"), dep);
+				}
+				
+				Department obj = instantiateDepartment(rs);
+				list.add(obj);
+			}
+			return list;
+		} 
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+	}
+
 	private Department instantiateDepartment(ResultSet rs) throws SQLException {
 		Department dep = new Department();
 		dep.setId(rs.getInt("Id"));
 		dep.setName(rs.getString("Name"));
 		return dep;
 	}
-
-	@Override
-	public List<Department> findAll() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+	
 }
